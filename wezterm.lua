e7c08f72d4c5e54affe6e32001799783c8691e11
rodpepablo <rodpepablo@gmail.com>
2025-04-12 01:26:57 +0200

    ADD: extra config (font, borders...)

--- wezterm.lua
+++ wezterm.lua
@@ -3,7 +3,12 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.color_scheme = "Catppuccin Frappe"
+config.font = wezterm.font("JetBrains Mono")
+
 config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+config.window_decorations = "RESIZE"
+
 config.window_background_opacity = 0.9
 
 return config
