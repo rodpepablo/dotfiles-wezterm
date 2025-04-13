911293f42a566b63d617fb571d91a4c01807fa27
rodpepablo <rodpepablo@gmail.com>
2025-04-13 20:06:45 +0200

    REMOVE: config for window decorations since using alt+enter goes fullwindow mode and already removes title bar (while keeping it in window mode)

--- wezterm.lua
+++ wezterm.lua
@@ -7,7 +7,6 @@ config.font = wezterm.font("JetBrains Mono")
 
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
-config.window_decorations = "RESIZE"
 
 config.window_background_opacity = 0.9
 
